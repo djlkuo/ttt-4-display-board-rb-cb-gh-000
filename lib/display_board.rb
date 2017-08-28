089d03593e03377ec1d46f38bb6130b8d6340fdf
Darren Kuo <wasd31627@gmail.com>
2017-08-28 09:30:43 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(p1="",p2="",p3="",p4='',p5='',p6='',p7='',p8='',p9='')
+def display_board(p1='',p2='',p3='',p4='',p5='',p6='',p7='',p8='',p9='')
   puts board = [" #{p1} | #{p2} | #{p3} ","-----------"," #{p4} | #{p5} | #{p6} ","-----------"," #{p7} | #{p8} | #{p9} "]
 end
-
-display_board("x","x","x")
