8e53a3e8f44076e83b968e72b05164d982cf2735
Darren Kuo <wasd31627@gmail.com>
2017-08-28 09:30:18 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,4 +5,4 @@ def display_board(p1="",p2="",p3="",p4='',p5='',p6='',p7='',p8='',p9='')
   puts board = [" #{p1} | #{p2} | #{p3} ","-----------"," #{p4} | #{p5} | #{p6} ","-----------"," #{p7} | #{p8} | #{p9} "]
 end
 
-display_board(x,x,x)
+display_board("x","x","x")
