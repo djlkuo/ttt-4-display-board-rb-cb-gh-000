cb34c230cf04135fe199aef718ce53f966db80d6
Darren Kuo <wasd31627@gmail.com>
2017-08-28 10:06:33 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board = [" "," "," "," "," "," "," "," "," "]
-
 def display_board(board)
   puts board
 end
 
+board = [" "," "," "," "," "," "," "," "," "]
 display_board(0)
