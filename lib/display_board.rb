1070c81841c55f201c2719e3b5e1897dd6ff1ca2
Darren Kuo <wasd31627@gmail.com>
2017-08-28 09:18:18 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 
 def display_board(p1='',p2='',p3='',p4='',p5='',p6='',p7='',p8='',p9='')
-  line1=[p1,p2,p3]
+  line1=p1,p2,p3
   line2=[p4,p5,p6]
   line3=[p7,p8,p9]
   cal=p1+p2
