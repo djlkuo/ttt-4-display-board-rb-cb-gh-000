ace528aef0b59985fd0cad34fe9186aaf9c4e0f9
Darren Kuo <wasd31627@gmail.com>
2017-08-28 09:53:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
+board = [" "," "," "," "," "," "," "," "," "]
+
 def display_board(board)
   rows = [" "+"#{p1}"+" | "+"#{p2}"+" | "+"#{p3}","-----------"," #{p4} | #{p5} | #{p6} ","-----------"," #{p7} | #{p8} | #{p9} "]
   puts rows[0]
