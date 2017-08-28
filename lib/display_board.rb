0bf226e243bdb4ca4070c839f6be70bea7aff9fc
Darren Kuo <wasd31627@gmail.com>
2017-08-28 09:35:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,3 +5,5 @@ def display_board(p1='',p2='',p3='',p4='',p5='',p6='',p7='',p8='',p9='')
   rows = ["   |   |   ","-----------","   |   |   ","-----------","   |   |   "]
   puts rows
 end
+
+display_board
