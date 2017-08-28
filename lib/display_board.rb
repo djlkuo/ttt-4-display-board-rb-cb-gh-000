4b345fa82d825b683142fddf0414e548ce1bbb9b
Darren Kuo <wasd31627@gmail.com>
2017-08-28 09:14:54 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@
 def display_board(p1='.',p2='.',p3='.',p4='.',p5='.',p6='.')
   line1=['p1','p2','p3']
   line2=[p4,p5,p6]
-  puts "#{line1}","#{lin2}","#{line3}"
+  puts "#{line1}","#{line2}","#{line3}"
 end
 
 display_board(1,2,3)
