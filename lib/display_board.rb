aa0e5bc81e1a8a8964aa0873e38c6cb4c658c137
Darren Kuo <wasd31627@gmail.com>
2017-08-28 09:08:28 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 
 def display_board(p1='.',p2='.',p3='.',p4='.',p5='.',p6='.')
-  line1=[p1,p2,p3]
+  line1=
   line2=[p4,p5,p6]
-  puts line1,line2
+  puts line1
 end
 
 display_board("x")
