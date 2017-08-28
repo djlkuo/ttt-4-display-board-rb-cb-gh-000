ce95476d1ff60a925212398d8f4729bea5687810
Darren Kuo <wasd31627@gmail.com>
2017-08-28 08:56:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(one='test',2=,3=,4=,5=,6=,7=,8=,9=)
+def display_board(p1='test',p2=,p3=,p4=,p5=,p6=,p7=,p8=,p9=)
   puts display_board
 
 end
