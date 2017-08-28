a237ec5dce5fad17e0170767222a038a339275f5
Darren Kuo <wasd31627@gmail.com>
2017-08-28 10:20:59 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 board = [" "," "," "," "," "," "," "," "," "]
 
-def display_board(board)
+def display_board(test)
   puts rows = [" "+board[0]+" | "+board[1]+" | "+board[2]+" ","-----------"," "+board[3]+" | "+board[4]+" | "+board[5]+" ","-----------"," "+board[6]+" | "+board[7]+" | "+board[8]+" "]
 end
 
